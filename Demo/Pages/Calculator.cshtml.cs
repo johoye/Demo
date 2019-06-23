8b3187bfdebcc107053909324ecdf9fa382483c0
Johan Høye <johan.hoye@gmail.com>
2019-06-23 21:37:58 +0200

    Add project files.

---
+++ Demo/Pages/Calculator.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Demo.Pages
+{
+    public class CalculatorModel : PageModel
+    {
+        [BindProperty]
+        public int Number1 { get; set; }
+
+        [BindProperty]
+        public int Number2 { get; set; }
+
+        [BindProperty]
+        public int Total { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
